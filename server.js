760e049ed0116abe82c7e5013e634b394fa53181
Tushar Samant <tsamant@architecture.org>
2023-06-15 17:35:10 -0500

    WIP Attempt CSRF token with cookies
    This is not right. It is just generating a token so far.

--- server.js
+++ server.js
@@ -3,18 +3,27 @@ const fastify = require('fastify')({
 })
 
 const path = require('path')
+const fs = require('fs')
+const replaceStream = require('replacestream')
 
-fastify.register(require('@fastify/static'), {
-	root: path.join(__dirname, 'public')
-})
-
-// fastify.register(require('@fastify/csrf-protection'))
+fastify.register(require('@fastify/cookie'))
+fastify.register(require('@fastify/csrf-protection'))
 fastify.register(require('@fastify/formbody'))
+
+fastify.get('/', async (req, res) => {
+	const stream = fs.createReadStream(
+		path.join(__dirname, 'public', 'text-drop.html'),
+		'utf8'
+	).pipe(replaceStream('%CSRF_TOKEN%', await res.generateCsrf()))
+
+	res.header('Content-Type', 'text/html')
+	await res.send(stream)
+})
 
 fastify.route({
 	method: 'POST',
 	path: '/text-drop',
-	// onRequest: fastify.csrfProtection,
+	onRequest: fastify.csrfProtection,
 	handler: async (req, res) => {
 		return req.body
 	}
