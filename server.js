5c9e71db1610d3c5a03fe76f4e5559563b06860f
Tushar Samant <live@now-is.org>
2023-06-20 10:21:01 -0500

    Back out the CSRF code
    Note that @fastify/static is also gone, for now.

--- server.js
+++ server.js
@@ -4,18 +4,14 @@ const fastify = require('fastify')({
 
 const path = require('path')
 const fs = require('fs')
-const replaceStream = require('replacestream')
 
-fastify.register(require('@fastify/cookie'))
-fastify.register(require('@fastify/csrf-protection'))
 fastify.register(require('@fastify/formbody'))
 
 fastify.get('/', async (req, res) => {
 	const stream = fs.createReadStream(
 		path.join(__dirname, 'public', 'text-drop.html'),
 		'utf8'
-	).pipe(replaceStream('%CSRF_TOKEN%', await res.generateCsrf()))
-
+	)
 	res.header('Content-Type', 'text/html')
 	await res.send(stream)
 })
@@ -23,7 +19,6 @@ fastify.get('/', async (req, res) => {
 fastify.route({
 	method: 'POST',
 	path: '/text-drop',
-	onRequest: fastify.csrfProtection,
 	handler: async (req, res) => {
 		/* TODO save to drops/`new Date() .toISOString()`.txt */
 		res.send(req.body)
