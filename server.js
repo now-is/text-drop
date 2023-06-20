63693f8771f4909cdf7aad46bd9c1f461a4e17a7
Tushar Samant <tsamant@architecture.org>
2023-06-20 10:56:00 -0500

    Write dropped text to disk

--- server.js
+++ server.js
@@ -17,8 +17,14 @@ fastify.route({
 	method: 'POST',
 	path: '/text-drop',
 	handler: async (req, res) => {
-		/* TODO save to drops/`new Date() .toISOString()`.txt */
-		res.send(req.body)
+		const drop_path = path.join(__dirname, 'drops', `${new Date() .toISOString()}.txt`)
+		try {
+			fs.writeFileSync(drop_path, req.body.dropped_text);
+		} catch (err) {
+			fastify.log.error(`Could not write to ${drop_path}`)
+			res.status(503).send({ ok: false })
+		}
+		res.send({ ok: true })
 	}
 })
 
