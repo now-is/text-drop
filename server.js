f9a7581d3bb994accba72cc7b366f158fbbfefd5
Tushar Samant <tsamant@architecture.org>
2023-06-20 13:59:57 -0500

    Exit on successful write; clean cruft

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ fastify.route({
 			fastify.log.error(`Could not write to ${drop_path}`)
 			res.status(503).send({ ok: false })
 		}
-		res.send({ ok: true })
+		await res.send({ ok: true })
+		process.exit(1)
 	}
 })
 
@@ -51,14 +52,3 @@ fastify.listen({ port: 3000 }, function (err, address) {
 		process.exit(1)
 	}
 })
-
-function send_html_file (public_filename) {
-	return async (req, res) => {
-		const stream = fs.createReadStream(
-			path.join(__dirname, 'public', public_filename),
-			'utf8'
-		)
-		res.header('Content-Type', 'text/html')
-		await res.send(stream)
-	}
-}
