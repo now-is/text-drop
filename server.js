daec27f10e378ec86ddc2ae7e0cedf08e907693c
Tushar Samant <tsamant@architecture.org>
2023-06-20 15:07:41 -0500

    Specify listen args in a file, log real home URL

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fastify = require('fastify')({
 const fs = require('fs')
 const path = require('path')
 const replaceStream = require('replacestream')
+const listenConfig = require('./listen_config.js')
 
 fastify.register(require('@fastify/formbody'))
 fastify.register(require('@fastify/static'), {
@@ -15,7 +16,6 @@ fastify.register(require('@fastify/static'), {
 const prefix_codes = new Uint8Array(8)
 crypto.getRandomValues(prefix_codes)
 const prefix = String.fromCharCode(... prefix_codes.map(n => { n >>= 3; return n + (n < 26 ? 97 : 22) }))
-fastify.log.info(`Planned prefix: ${prefix}`)
 
 fastify.get(`/${prefix}/succeeded`, (req, res) => res.sendFile('text-drop-succeeded.html'))
 fastify.get(`/${prefix}/failed`,    (req, res) => res.sendFile('text-drop-failed.html'))
@@ -46,7 +46,12 @@ fastify.route({
 	}
 })
 
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({
+	... listenConfig,
+	listenTextResolver: address => {
+		return `Listening at ${address}/${prefix}/`
+	}
+}, function (err, address) {
 	if (err) {
 		fastify.log.error(err)
 		process.exit(1)
